7bfccbe7ab67f90d5a0abfe27b48eb7396f1ad5f
zhangjunxiongjie <zhangjunxiongjie@163.com>
2021-01-19 22:08:04 -0800

    add program function

---
+++ program-function/arithmetic.c
@@ -0,0 +1,48 @@
+
+#include "arithmetic.h"
+
+void Next(const char *T, int *next){
+	int i = 1;
+	next[1] = 0;
+	int j = 0;
+	while (i < strlen(T)){
+		if ((j == 0) || (T[i - 1] == T[j - 1])){
+			i++;
+			j++;
+			next[i] = j;
+		}
+		else{
+			j = next[j];
+		}
+	}
+}
+
+int KMP(const char *S, const char *T)
+{
+	// printf("%s", S);
+
+    int next[strlen(T)];
+    Next(T, next);
+
+    int i=1;
+    int j=1;
+    while (i<=strlen(S)&&j<=strlen(T)) {
+                
+        if (j==0 || S[i-1]==T[j-1]) {
+            i++;
+            j++;
+        }
+        else{
+            j=next[j];
+        }
+    }
+    if (j>strlen(T)) {
+        return i-(int)strlen(T);
+    }
+
+    return -1;
+}
+
+
+
+
---
+++ program-function/arithmetic.h
@@ -0,0 +1,13 @@
+#ifndef ARITHMETIC_H
+#define ARITHMETIC_H
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+// string match
+// match finsh return > 0 else return == -1
+int KMP(const char *S, const char *T);
+
+
+#endif
