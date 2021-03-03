5772c689da08bfb1b5100cfd1dfb353fc3de5e1f
murongyuyue <zhangjunxiongjie@gamil.com>
2021-03-03 14:49:04 +0800

    add ohmyzsh

--- config-and-shortcuts-and-command/linux_environment/linux_config_script.sh
+++ config-and-shortcuts-and-command/linux_environment/linux_config_script.sh
@@ -1,9 +1,12 @@
 
 
 # install 'oh my zsh' pulg manage of zsh.
-wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O ~/Downloads/ohmyzsh
+# wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O ~/Downloads/ohmyzsh
+#$(~/Downloads/ohmyzsh)
 
-$(~/Downloads/ohmyzsh)
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+chsh -s /bin/zsh
 
 # ? -> whatis command. 
 
