144cb6f571c25e2f51ba8be7fe1225d1c6678e32
zhangjunxiongjie <zhangjunxiongjie@163.com>
2021-03-03 00:50:06 -0800

    edit shell script

--- config-and-shortcuts-and-command/linux_environment/linux_config_script.sh
+++ config-and-shortcuts-and-command/linux_environment/linux_config_script.sh
@@ -26,8 +26,8 @@ echo 'configuration shell successful!!!'
 # cat 'alias ?=\'cowsay -f $(ls /usr/share/cowsay/cows | shuf -n 1 | cut -d. -f1) $(whatis $(ls /bin) 2>/dev/null | shuf -n 1)\'' >> ~/.bashrc
 
 # sudo apt install cowsay
-# cat 'cowsay -f $(ls /usr/share/cowsay/cows | shuf -n 1 | cut -d. -f1) $(whatis $(ls /bin) 2>/dev/null | shuf -n 1)' >> ~/.zshrc
-# cat 'alias ?=\'cowsay -f $(ls /usr/share/cowsay/cows | shuf -n 1 | cut -d. -f1) $(whatis $(ls /bin) 2>/dev/null | shuf -n 1)\'' >> ~/.zshrc
+# cowsay -f $(ls /usr/share/cowsay/cows | shuf -n 1 | cut -d. -f1) $(whatis $(ls /bin) 2>/dev/null | shuf -n 1) # >> ~/.zshrc
+# alias '?'='cowsay -f $(ls /usr/share/cowsay/cows | shuf -n 1 | cut -d. -f1) $(whatis $(ls /bin) 2>/dev/null | shuf -n 1)' # >> ~/.zshrc
 
 
 
