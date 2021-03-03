015017edddfecb7dcacc146d3a678302146d731e
zhangjunxiongjie <zhangjunxiongjie@163.com>
2021-03-03 00:03:04 -0800

    edit shell script

--- config-and-shortcuts-and-command/linux_environment/linux_config_script.sh
+++ config-and-shortcuts-and-command/linux_environment/linux_config_script.sh
@@ -2,15 +2,15 @@
 # config vim
 sudo cp -r ./vim_runtime /opt/
 sudo chmod a+x /opt/vim_runtime/install_awesome_parameterized.sh
-sudo /opt/vim_runtime/install_awesome_parameterized.sh /opt/runtime --all
+sudo /opt/vim_runtime/install_awesome_parameterized.sh /opt/run_time --all
 
 # install 'oh my zsh' pulg manage of zsh.
 # wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O ~/Downloads/ohmyzsh
 #$(~/Downloads/ohmyzsh)
 
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh #
-cp ./zsh_config_file.txt  ~/.zshrc # config
-cp -r ./zsh_plugins/* ~/.oh-my-zsh/custom/plugins/ #plug
+cp ./zsh_config_file.txt ~/.zshrc # config
+cp -r ./zsh-plugins/* ~/.oh-my-zsh/custom/plugins/ #plug
 sudo apt install zsh # shell
 chsh -s /bin/zsh # chang default shell
 
